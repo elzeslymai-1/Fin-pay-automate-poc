30fdd1b7802cbd58066c651e0159f6a8acd2ae8d
Armmu <karis.arm13@gmail.com>
2023-12-19 11:03:01 +0700

    feature: splash screen test script
    ++ splash screen components
    ++ splash screen action/assert
    ++ splash screen test cases
    ++ action: launchAndroidApp
    ++ config: wdio.andorid

---
+++ androidTest/mainComponent/component/splashScreenComponent.ts
@@ -0,0 +1,20 @@
+export class splashScreenComponent {
+    
+    //  Action Component
+    splashScreenCreateWalletBtn = `android=new UiSelector().text("Create a new wallet").className("android.widget.TextView")`
+    splashScreenWelcomeText = `android=new UiSelector().text("Welcome to the \nFIN PAY").className("android.widget.TextView")`
+    splashScreenImportWalletBtn = `android=new UiSelector().text("Import a wallet").className("android.widget.TextView")`
+    splashScreenDescriptionText = `android=new UiSelector().text("To get started, create a new wallet or import \none from a seed phrase.").className("android.widget.TextView")`
+
+
+    createWalletTitleText = `android=new UiSelector().text("Create wallet").className("android.view.View")`
+    createWalletBackBtn = `android=new UiSelector().text("").className("android.widget.TextView")`
+    importWalletTitleText = `android=new UiSelector().text("Import Wallet").className("android.view.View")`
+    importWalletBackBtn = `android=new UiSelector().text("").className("android.widget.TextView")`
+
+    //  Assert Component
+    // assertTabCreateNewWalletBtn = 'XCUIElementTypeStaticText'
+    // assertTabImportWalletBtn = '-ios class chain:**/XCUIElementTypeStaticText[`name == "Import Wallet"`]'
+}
+
+
---
+++ androidTest/specs/splashScreenPage.e2e.ts
@@ -0,0 +1,63 @@
+import { expect } from '@wdio/globals'
+import { splashScreenComponent } from '../mainComponent/component/splashScreenComponent.js'
+import { Action } from '../mainComponent/mainFunction/Action.js'
+import { Assertion } from '../mainComponent/mainFunction/Assert.js'
+
+const component = new splashScreenComponent()
+const action = new Action()
+const assert = new Assertion()
+
+describe('Splash Screen Page Scenario', () => {
+    beforeAll(async () => {
+        //await action.launchApps(`${process.env.BUNDLE_ID}`)
+        // await action.launchAppsAndroid(`${process.env.BUNDLE_ID}`)
+        await action.launchAppsAndroid(`${process.env.BUNDLE_ID}`)
+    })
+
+    it('[Wording] => Check Welcome', async () => {
+        await action.waitForDisplayed(component.splashScreenWelcomeText)
+        await assert.checkText(component.splashScreenWelcomeText, 'Welcome to the \nFIN PAY')
+    })
+
+    it('[Wording] => Check Description', async () => {
+        await action.waitForDisplayed(component.splashScreenDescriptionText)
+        await assert.checkText(component.splashScreenDescriptionText, 'To get started, create a new wallet or import \none from a seed phrase.')
+    })
+
+    it('[Display] => Check Create wallet Btn', async () => {
+        await action.waitForDisplayed(component.splashScreenCreateWalletBtn)
+        await assert.checkText(component.splashScreenCreateWalletBtn, 'Create a new wallet')
+    })
+
+    it('[Display] => Check Import wallet Btn', async () => {
+        await action.waitForDisplayed(component.splashScreenImportWalletBtn)
+        await assert.checkText(component.splashScreenImportWalletBtn, 'Import a wallet')
+    })
+
+    it('[Tap] => Check Create wallet Btn', async () => {
+        await action.waitForDisplayed(component.splashScreenCreateWalletBtn)
+        await action.click(component.splashScreenCreateWalletBtn)
+
+        await action.waitForDisplayed(component.createWalletTitleText)
+        await assert.checkText(component.createWalletTitleText, 'Create wallet')
+
+        await action.waitForDisplayed(component.createWalletBackBtn)
+        await action.click(component.createWalletBackBtn)
+    })
+
+    it('[Tap] => Check Import wallet Btn', async () => {
+        await action.waitForDisplayed(component.splashScreenImportWalletBtn)
+        await action.click(component.splashScreenImportWalletBtn)
+
+        await action.waitForDisplayed(component.importWalletTitleText)
+        await assert.checkText(component.importWalletTitleText, 'Import Wallet')
+
+        await action.waitForDisplayed(component.importWalletBackBtn)
+        await action.click(component.importWalletBackBtn)
+    })
+
+    afterAll(async () => {
+        await action.closeApps(`${process.env.BUNDLE_ID}`)
+        //await action.removeApps('com.finstabledev.finsdefiwallet')
+    })
+})
