ece865ea2c44053830e91f0a4ac5e736dfd20f6a
Juggrid <champ_psu@outlook.co.th>
2024-01-05 17:15:48 +0700

    Refactor: import wallet ios

--- test/iosTest/specs/importWalletPage.e2e.ts
+++ test/iosTest/specs/importWalletPage.e2e.ts
@@ -6,70 +6,62 @@ const component = new importWalletComponent()
 const action = new Action()
 const assert = new Assertion()
 
-describe('Import Wallet Page Senario', () => {
+describe('Import Wallet Page Scenario', () => {
     beforeAll(async () => {
         await action.launchAndroidApps(`${process.env.BUNDLE_ID}`)
         await action.click(component.splashScreenImportWalletBtn)
     })
 
-    // === Check Import Wallet Page ===
-    it('[Wording] Check Import Wallet Header', async () => {
+    it('[Display] Check Private Key Field Displayed', async () => {
         //assert
-        await action.waitForDisplayed(component.importWalletTitleText)
-        await assert.checkText(component.importWalletTitleText, 'Import Wallet')
-    })
-
-    it('[Wording] Check Import Wallet Topic', async () => {
-        //assert
-        await action.waitForDisplayed(component.importWalletHeaderText)
-        await assert.checkText(component.importWalletHeaderText, 'Enter Mnemonic or Private Key')
-    })
-
-    it('[Wording] Check Import Wallet Description', async () => {
-        //assert
-        await action.waitForDisplayed(component.importWalletDescriptionText)
-        await assert.checkText(component.importWalletDescriptionText, 'Input your mnemonic phrases with spacing. Supports 12-word, 24-word mnemonic phrases and private keys for all wallet types.')
-    })
-
-    it('[Display] Check Back Btn', async () => {
-        //assert
-        await action.waitForDisplayed(component.importWalletBackBtn)
-        await assert.checkElementDisplayed(component.importWalletBackBtn)
-        await assert.checkText(component.importWalletBackBtn, '')
-    })
-
-    it('[Display] Check Private Key Field', async () => {
-        //assert
-        await action.waitForDisplayed(component.importWalletPrivatekeyField)
         await assert.checkElementDisplayed(component.importWalletPrivatekeyField)
     })
 
-    it('[Display] Check Paste Btn', async () => {
+    it('[Display] Check Paste Btn Displayed', async () => {
         //assert
-        await action.waitForDisplayed(component.importWalletPrivatekeyField)
         await assert.checkElementDisplayed(component.importWalletPrivatekeyField)
     })
 
-    it('[Display] Check Import Wallet Btn', async () => {
+    it('[Display] Check Import Wallet Btn Displayed', async () => {
         //assert
-        await action.waitForDisplayed(component.importWalletImportBtn)
         await assert.checkElementDisplayed(component.importWalletImportBtn)
-        await assert.checkText(component.importWalletImportBtn, 'Import Wallet')
     })
-
 
     it('[Display] Check Import Wallet Btn Should be Disabled', async () => {
         //assert
-        await action.waitForDisplayed(component.importWalletImportBtn)
+        await assert.checkElementDisplayed(component.importWalletImportBtn)
         await assert.checkDisabled(component.importWalletImportBtn)
     })
 
+    it('[Wording] Check Import Wallet Header Wording', async () => {
+        //assert
+        await assert.checkText(component.importWalletTitleText, 'Import Wallet')
+    })
+
+    it('[Wording] Check Import Wallet Topic Wording', async () => {
+        //assert
+        await assert.checkText(component.importWalletHeaderText, 'Enter Mnemonic or Private Key')
+    })
+
+    it('[Wording] Check Import Wallet Description Wording', async () => {
+        //assert
+        await assert.checkText(component.importWalletDescriptionText, 'Input your mnemonic phrases with spacing. Supports 12-word, 24-word mnemonic phrases and private keys for all wallet types.')
+    })
+
+    it('[Wording] Check Back Btn Wording', async () => {
+        //assert
+        await assert.checkText(component.importWalletBackBtn, '')
+    })
+
+    it('[Wording] Check Import wallet Btn Wording', async () => {
+        // assert
+        await assert.checkText(component.importWalletImportBtn, 'Import Wallet')
+    })
+
     it('[Tap] Check Back Btn Tap', async () => {
-        await action.waitForDisplayed(component.importWalletBackBtn)
         await action.click(component.importWalletBackBtn)
 
         //assert
-        await action.waitForDisplayed(component.splashScreenImportWalletBtn)
         await assert.checkElementDisplayed(component.splashScreenImportWalletBtn)
 
         //after
@@ -78,14 +70,10 @@ describe('Import Wallet Page Senario', () => {
 
     it('[Tap] Check Paste Btn Tap', async () => {
         await action.setClipboard(`${process.env.MNEMONIC_PHRASE}`)
-
-        await action.waitForDisplayed(component.importWalletPasteBtn)
         await action.click(component.importWalletPasteBtn)
-
         await action.acceptAlert()
 
         //assert
-        await action.waitForDisplayed(component.importWalletImportBtn)
         await assert.checkEnabled(component.importWalletImportBtn)
 
         //after
@@ -93,21 +81,14 @@ describe('Import Wallet Page Senario', () => {
     })
 
     it('[Functional] Check Mnemonic Phrase Wrong Format', async () => {
-        await action.waitForDisplayed(component.splashScreenImportWalletBtn)
         await action.click(component.splashScreenImportWalletBtn)
 
         await action.setClipboard('test test')
-
-        await action.waitForDisplayed(component.importWalletPasteBtn)
         await action.click(component.importWalletPasteBtn)
-
         await action.acceptAlert()
 
         //assert
-        await action.waitForDisplayed(component.importWalletMnemonicErrorMessageText)
         await assert.checkText(component.importWalletMnemonicErrorMessageText, 'Invalid mnemonic phrase.')
-
-        await action.waitForDisplayed(component.importWalletImportBtn)
         await assert.checkDisabled(component.importWalletImportBtn)
 
         //after
@@ -115,24 +96,16 @@ describe('Import Wallet Page Senario', () => {
     })
 
     it('[Functional] Check Import Wallet Failed', async () => {
-        await action.waitForDisplayed(component.splashScreenImportWalletBtn)
         await action.click(component.splashScreenImportWalletBtn)
 
         await action.setClipboard('t t t t t t t t t t t t')
-
-        await action.waitForDisplayed(component.importWalletPasteBtn)
         await action.click(component.importWalletPasteBtn)
-
         await action.acceptAlert()
 
-        await action.waitForDisplayed(component.importWalletImportBtn)
         await action.click(component.importWalletImportBtn)
 
         //assert
-        await action.waitForDisplayed(component.importWalletMnemonicErrorMessageText)
         await assert.checkText(component.importWalletMnemonicErrorMessageText, 'Invalid mnemonic phrase.')
-
-        await action.waitForDisplayed(component.importWalletImportBtn)
         await assert.checkDisabled(component.importWalletImportBtn)
 
         //after
@@ -140,35 +113,24 @@ describe('Import Wallet Page Senario', () => {
     })
 
     it('[Functional] Check Import Wallet Success', async () => {
-        await action.waitForDisplayed(component.splashScreenImportWalletBtn)
         await action.click(component.splashScreenImportWalletBtn)
 
-        // await action.setClipboard('t t t t t t t t t t t t')
         await action.setClipboard(`${process.env.MNEMONIC_PHRASE}`)
-
-        await action.waitForDisplayed(component.importWalletPasteBtn)
         await action.click(component.importWalletPasteBtn)
-
         await action.acceptAlert()
 
-        await action.waitForDisplayed(component.importWalletImportBtn)
         await action.click(component.importWalletImportBtn)
 
         //assert
-        await action.waitForDisplayed(component.importWalletLoadingText)
         await assert.checkText(component.importWalletLoadingText, 'LOADING WALLET...')
-
         await action.pause(3000)
-
-        await action.waitForDisplayed(component.importWalletSuccessText)
         await assert.checkText(component.importWalletSuccessText, 'IMPORT WALLET SUCCESS')
-
         await action.pause(3000)
-
         await assert.checkElementDisplayed(component.homeTabHomeBtn)
     })
 
     afterAll(async () => {
+        // remove apps
         await action.removeApps(`${process.env.BUNDLE_ID}`)
     })
 })
