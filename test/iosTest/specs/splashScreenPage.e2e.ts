9cd108a470e4a0c378fe188d8c5a4bf264b7af94
Juggrid <champ_psu@outlook.co.th>
2024-01-05 17:00:40 +0700

    Refactor: splash screen ios

--- test/iosTest/specs/splashScreenPage.e2e.ts
+++ test/iosTest/specs/splashScreenPage.e2e.ts
@@ -11,15 +11,23 @@ describe('Splash Screen Page Scenario', () => {
         await action.launchApps(`${process.env.BUNDLE_ID}`)
     })
 
+    it('[Displayed] => Create New Wallet Btn Should be Displayed',async ()=>{
+        //assert
+        await assert.checkElementDisplayed(component.splashScreenCreateWalletBtn)
+    })
+
+    it('[Displayed] => Import Wallet Btn Should be Displayed',async ()=>{
+        //assert
+        await assert.checkElementDisplayed(component.splashScreenImportWalletBtn)
+    })
+
     it('[Wording] => Check Welcome Page Wording [1]', async () => {    
         //assert
-        await action.waitForDisplayed(component.splashScreenWelcomeText1)
         await assert.checkText(component.splashScreenWelcomeText1, 'Welcome to the \nFIN PAY')
     })
 
     it('[Wording] => Check Description Page Wording [1]', async () => {
         //assert
-        await action.waitForDisplayed(component.splashScreenDescriptionText1)
         await assert.checkText(component.splashScreenDescriptionText1, 'To get started, create a new wallet or import \none from a seed phrase.')
     })
 
@@ -27,13 +35,11 @@ describe('Splash Screen Page Scenario', () => {
         await action.swipe({x:400, y:300},{x:50, y:300})
     
         //assert
-        await action.waitForDisplayed(component.splashScreenWelcomeText2)
         await assert.checkText(component.splashScreenWelcomeText2, 'All your crypto in one place')
     })
 
     it('[Wording] => Check Description Page Wording [2]', async () => {
         //assert
-        await action.waitForDisplayed(component.splashScreenDescriptionText2)
         await assert.checkText(component.splashScreenDescriptionText2, 'Now you can trade crypto directly from the\nCrypto wallet with your existing.')
     })
 
@@ -41,34 +47,28 @@ describe('Splash Screen Page Scenario', () => {
         await action.swipe({x:400, y:300},{x:50, y:300})
     
         //assert
-        await action.waitForDisplayed(component.splashScreenWelcomeText3)
         await assert.checkText(component.splashScreenWelcomeText3, 'Discover the best of Web3')
     })
 
     it('[Wording] => Check Description Page Wording [3]', async () => {
         //assert
-        await action.waitForDisplayed(component.splashScreenDescriptionText3)
         await assert.checkText(component.splashScreenDescriptionText3, 'Find and connect to leading apps and\ncollections with the in-app browser.')
     })
 
-    it('[Displayed] => Create New Wallet Btn Should be Displayed',async ()=>{
+    it('[Wording] => Check Create New Wallet Btn Wording',async ()=>{
         //assert
-        await action.waitForDisplayed(component.splashScreenCreateWalletBtn)
-        await assert.checkElementDisplayed(component.splashScreenCreateWalletBtn)
+        await assert.checkText(component.splashScreenCreateWalletBtn,'Create a new wallet')
     })
 
-    it('[Displayed] => Import Wallet Btn Should be Displayed',async ()=>{
+    it('[Wording] => Check Import Wallet Btn Wording',async ()=>{
         //assert
-        await action.waitForDisplayed(component.splashScreenImportWalletBtn)
-        await assert.checkElementDisplayed(component.splashScreenImportWalletBtn)
+        await assert.checkText(component.splashScreenImportWalletBtn,'Import a wallet')
     })
 
     it('[Tap] => Create new Wallet Btn', async () => {
-        await action.waitForDisplayed(component.splashScreenCreateWalletBtn)
         await action.click(component.splashScreenCreateWalletBtn)
 
         //assert
-        await action.waitForDisplayed(component.createNewWalletHeaderText)
         await assert.checkText(component.createNewWalletHeaderText, 'Create Wallet')
 
         //after
@@ -76,11 +76,9 @@ describe('Splash Screen Page Scenario', () => {
     })
 
     it('[Tap] => Import Wallet Btn', async () => {
-        await action.waitForDisplayed(component.splashScreenImportWalletBtn)
         await action.click(component.splashScreenImportWalletBtn)
 
         //assert
-        await action.waitForDisplayed(component.importWalletHeaderText)
         await assert.checkText(component.importWalletHeaderText,'Import Wallet')
 
         //after
@@ -88,6 +86,7 @@ describe('Splash Screen Page Scenario', () => {
     })
 
     afterAll(async () => {
+        // close apps
         await action.closeApps(`${process.env.BUNDLE_ID}`)
     })
 })
