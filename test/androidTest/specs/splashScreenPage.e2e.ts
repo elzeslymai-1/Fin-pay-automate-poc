ad70be5a127c93967d63838c270dc49b1874fd5b
Juggrid <champ_psu@outlook.co.th>
2024-01-08 10:00:42 +0700

    Refactor: Splash Screen Android

--- test/androidTest/specs/splashScreenPage.e2e.ts
+++ test/androidTest/specs/splashScreenPage.e2e.ts
@@ -1,4 +1,3 @@
-import { expect } from '@wdio/globals'
 import { splashScreenComponent } from '../component/splashScreenComponent.js'
 import { Action } from '../../mainComponent/mainFunction/Action.js'
 import { Assertion } from '../../mainComponent/mainFunction/Assert.js'
@@ -12,49 +11,48 @@ describe('Splash Screen Page Scenario', () => {
         await action.launchAndroidApps(`${process.env.PACKAGE_ID}`)
     })
 
-    it('[Wording] => Check Welcome', async () => {
-        await action.waitForDisplayed(component.splashScreenWelcomeText)
-        await assert.checkText(component.splashScreenWelcomeText, 'Welcome to the \nFIN PAY')
-    })
-
-    it('[Wording] => Check Description', async () => {
-        await action.waitForDisplayed(component.splashScreenDescriptionText)
-        await assert.checkText(component.splashScreenDescriptionText, 'To get started, create a new wallet or import \none from a seed phrase.')
-    })
-
     it('[Display] => Check Create wallet Btn', async () => {
-        await action.waitForDisplayed(component.splashScreenCreateWalletBtn)
+        // assert
         await assert.checkText(component.splashScreenCreateWalletBtn, 'Create a new wallet')
     })
 
     it('[Display] => Check Import wallet Btn', async () => {
-        await action.waitForDisplayed(component.splashScreenImportWalletBtn)
+        // assert
         await assert.checkText(component.splashScreenImportWalletBtn, 'Import a wallet')
     })
 
+    it('[Wording] => Check Welcome', async () => {
+        // assert
+        await assert.checkText(component.splashScreenWelcomeText, 'Welcome to the \nFIN PAY')
+    })
+
+    it('[Wording] => Check Description', async () => {
+        // assert
+        await assert.checkText(component.splashScreenDescriptionText, 'To get started, create a new wallet or import \none from a seed phrase.')
+    })
+
     it('[Tap] => Create wallet Btn', async () => {
-        await action.waitForDisplayed(component.splashScreenCreateWalletBtn)
         await action.click(component.splashScreenCreateWalletBtn)
 
-        await action.waitForDisplayed(component.createWalletTitleText)
+        // assert
         await assert.checkText(component.createWalletTitleText, 'Create Wallet')
 
-        await action.waitForDisplayed(component.createWalletBackBtn)
+        // after
         await action.click(component.createWalletBackBtn)
     })
 
     it('[Tap] => Import wallet Btn', async () => {
-        await action.waitForDisplayed(component.splashScreenImportWalletBtn)
         await action.click(component.splashScreenImportWalletBtn)
 
-        await action.waitForDisplayed(component.importWalletTitleText)
+        // assert
         await assert.checkText(component.importWalletTitleText, 'Import Wallet')
 
-        await action.waitForDisplayed(component.importWalletBackBtn)
+        // after
         await action.click(component.importWalletBackBtn)
     })
 
     afterAll(async () => {
+        // close apps
         await action.closeApps(`${process.env.PACKAGE_ID}`)
     })
 })
--- wdio.android.conf.ts
+++ wdio.android.conf.ts
@@ -37,14 +37,14 @@ export const config: Options.Testrunner = {
         [
             // "./test/androidTest/specs/**.e2e.ts"
             "./test/androidTest/specs/splashScreenPage.e2e.ts",
-            "./test/androidTest/specs/createWalletPage.e2e.ts",
-            "./test/androidTest/specs/backUpPage.e2e.ts",
-            "./test/androidTest/specs/importWalletPage.e2e.ts",
-            "./test/androidTest/specs/homePage.e2e.ts",
-            "./test/androidTest/specs/settingPage.e2e.ts",
-            "./test/androidTest/specs/currencyPage.e2e.ts",
-            "./test/androidTest/specs/customNetworkPage.e2e.ts",
-            "./test/androidTest/specs/customTokenPage.e2e.ts",
+            // "./test/androidTest/specs/createWalletPage.e2e.ts",
+            // "./test/androidTest/specs/backUpPage.e2e.ts",
+            // "./test/androidTest/specs/importWalletPage.e2e.ts",
+            // "./test/androidTest/specs/homePage.e2e.ts",
+            // "./test/androidTest/specs/settingPage.e2e.ts",
+            // "./test/androidTest/specs/currencyPage.e2e.ts",
+            // "./test/androidTest/specs/customNetworkPage.e2e.ts",
+            // "./test/androidTest/specs/customTokenPage.e2e.ts",
         ]
     ],
     // Patterns to exclude.
