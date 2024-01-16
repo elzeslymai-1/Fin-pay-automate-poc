bfcdf097f5846810630eb82322403c928b1e9d40
Arm-mu <karis.arm13@gmail.com>
2024-01-16 15:01:14 +0700

    Refactor: custom network android

--- test/androidTest/component/customNetworkComponent.ts
+++ test/androidTest/component/customNetworkComponent.ts
@@ -1,34 +1,57 @@
 export class customNetworkComponent {
-    // BeforeAll Components
-    homeSettingBtn = `android=new UiSelector().text("Setting").className("android.widget.TextView")`
-    settingTitleText = `android=new UiSelector().text("Setting").className("android.view.View")`
-    homeBtn = `android=new UiSelector().text("Home").className("android.widget.TextView")`
-    settingNetworkManageBtn = `android=new UiSelector().text("Network Management").className("android.widget.TextView")`
 
     // Network Management Component
     networkmanagementTitleText = `android=new UiSelector().text("Add Network").className("android.view.View")`
-    networkmanagementCloseIconBtn = `android=new UiSelector().text("").className("android.widget.TextView")`
+    networkmanagementXBtn = `android=new UiSelector().text("").className("android.widget.TextView")`
     networkmanagementPopularTab = `android=new UiSelector().text("POPULAR").className("android.widget.TextView")`
-    
+
     networkmanagementCustomTab = `android=new UiSelector().text("CUSTOM NETWORK").className("android.widget.TextView")`
-    networkmanagementAddcustomnetBtn = `android=new UiSelector().text("Add Custom Network").className("android.widget.TextView")`
-    networkmanagementRemoveCustomnetBtn = `android=new UiSelector().text("").className("android.widget.TextView")`
-    
+
+    // Popular Tab Component
+    popularTabBitkubChainTestnet = `android=new UiSelector().text("Bitkub Chain Testnet").className("android.widget.TextView")`
+
+    // Custom Network Tab Component
+    customTabAddcustomnetBtn = `android=new UiSelector().text("Add Custom Network").className("android.widget.TextView")`
+    customTabGoerliRemoveBtn = `android=new UiSelector().text("").className("android.widget.TextView")`
+
     // Confirm Remove Pop-up Component
-    confirmremoveBtn = `android=new UiSelector().text("Remove").className("android.widget.TextView")`
-    confirmremoveCancelBtn = `android=new UiSelector().text("Cancel").className("android.widget.TextView")`
+    confirmRemoveTitleText = `android=new UiSelector().text("Remove goerli Chain").className("android.widget.TextView")`
+    confirmRemoveText = `android=new UiSelector().text("Assets on removed networks will no longer be displayed.").className("android.widget.TextView")`
+
+    confirmRemoveRemoveBtn = `android=new UiSelector().text("Remove").className("android.widget.TextView")`
+    confirmRemoveCancelBtn = `android=new UiSelector().text("Cancel").className("android.widget.TextView")`
 
     // Custom Network Component
     customnetTitleText = `android=new UiSelector().text("Custom Network").className("android.view.View")`
     customnetBackBtn = `android=new UiSelector().text("").className("android.widget.TextView")`
-    customnetCloseIconBtn = `android=new UiSelector().text("").className("android.widget.TextView")`
-    
+    customnetXBtn = `android=new UiSelector().text("").className("android.widget.TextView")`
+    customnetWarningText = `android=new UiSelector().text("A Malicious network provider can lie about the state of the blockchain and record your network activity. Only add networks you trust.").className("android.widget.TextView")`
+
+    customnetNetworkNameText = `android=new UiSelector().text("Network Name").className("android.widget.TextView")`
+    customnetNetworkNameInfoBtn = `android=new UiSelector().text("").className("android.widget.TextView").index(2)`
+    customnetNetworkNameInfoText = `android=new UiSelector().text("The name associated with this network.").className("android.widget.TextView")`
     customnetNetworkNameTextField = `android=new UiSelector().className("android.widget.EditText").index(3)`
+
+    customnetNetworkURLText = `android=new UiSelector().text("Network URL").className("android.widget.TextView")`
+    customnetNetworkURLInfoBtn = `android=new UiSelector().text("").className("android.widget.TextView").index(6)`
+    customnetNetworkURLInfoText = `android=new UiSelector().text("The URL used to access this network.").className("android.widget.TextView")`
     customnetNetworkUrlTextField = `android=new UiSelector().className("android.widget.EditText").index(7)`
-    customnetChainIdTextField = `android=new UiSelector().className("android.widget.EditText").index(11)`
-    customnetSymbolTextField = `android=new UiSelector().className("android.widget.EditText").index(15)`
-    customnetBlockExplorTextField = `android=new UiSelector().className("android.widget.EditText").index(19)`
+
+    customnetChainIDText = `android=new UiSelector().text("Chain ID").className("android.widget.TextView")`
+    customnetChainIDInfoBtn = `android=new UiSelector().text("").className("android.widget.TextView").index(10)`
+    customnetChainIDInfoText = `android=new UiSelector().text("The chain ID used to sign transactions for this network.").className("android.widget.TextView")`
+    customnetChainIDTextField = `android=new UiSelector().className("android.widget.EditText").index(11)`
     
+    customnetSymbolText = `android=new UiSelector().text("Symbol").className("android.widget.TextView")`
+    customnetSymbolInfoBtn = `android=new UiSelector().text("").className("android.widget.TextView").index(14)`
+    customnetSymbolInfoText = `android=new UiSelector().text("The ticker symbol displayed for this network’s currency.").className("android.widget.TextView")`
+    customnetSymbolTextField = `android=new UiSelector().className("android.widget.EditText").index(15)`
+    
+    customnetBlockExplorText = `android=new UiSelector().text("Block Explorer URL (Optional)").className("android.widget.TextView")`
+    customnetBlockExplorInfoBtn = `android=new UiSelector().text("").className("android.widget.TextView").index(18)`
+    customnetBlockExplorInfoText = `android=new UiSelector().text("The URL used as the block explorer for this network.").className("android.widget.TextView")`
+    customnetBlockExplorTextField = `android=new UiSelector().className("android.widget.EditText").index(19)`
+
     customnetCancelBtn = `android=new UiSelector().text("Cancel").className("android.widget.TextView")`
     customnetConfirmBtn = `android=new UiSelector().text("Confirm").className("android.widget.TextView")`
 
@@ -42,5 +65,16 @@ export class customNetworkComponent {
 
     // Home with Goerli chain Component
     homeGoerlichain = `android=new UiSelector().text("goerli").className("android.widget.TextView")`
+    homeETHToken = `android=new UiSelector().text("ETH").className("android.widget.TextView")`
+
+    // BeforeAll Component
+    homeSettingBtn = `android=new UiSelector().text("Setting").className("android.widget.TextView")`
+    settingTitleText = `android=new UiSelector().text("Setting").className("android.view.View")`
+    homeBtn = `android=new UiSelector().text("Home").className("android.widget.TextView")`
+    settingNetworkManageBtn = `android=new UiSelector().text("Network Management").className("android.widget.TextView")`
+    splashScreenCreateWalletBtn = `android=new UiSelector().text("Create a new wallet").className("android.widget.TextView")`
+    createWallet12wordBtn = `android=new UiSelector().text("12-word Mnemonic wallet").className("android.widget.TextView")`
+    createWalletContinueBtn = `android=new UiSelector().text("Continue").className("android.widget.TextView")`
+    successContinueBtn = `android=new UiSelector().text("Continue").className("android.widget.TextView")`
 
 }
--- test/androidTest/specs/customNetworkPage.e2e.ts
+++ test/androidTest/specs/customNetworkPage.e2e.ts
@@ -1,4 +1,3 @@
-import { expect } from '@wdio/globals'
 import { customNetworkComponent } from '../component/customNetworkComponent.js'
 import { Action } from '../../mainComponent/mainFunction/Action.js'
 import { Assertion } from '../../mainComponent/mainFunction/Assert.js'
@@ -7,115 +6,466 @@ const component = new customNetworkComponent()
 const action = new Action()
 const assert = new Assertion()
 
-describe('Custom Network Page Test Senario', () => {
+describe('Network Management Page Test Scenario', () => {
     beforeAll(async () => {
+        await action.installApps(`${process.env.PATH_APK}`)
         await action.launchAndroidApps(`${process.env.PACKAGE_ID}`)
 
-        await action.waitForDisplayed(component.homeSettingBtn)
+        await action.click(component.splashScreenCreateWalletBtn)
+
+        await action.click(component.createWallet12wordBtn)
+
+        await action.click(component.createWalletContinueBtn)
+        await action.pause(3000)
+
+        await action.click(component.successContinueBtn)
 
         await action.click(component.homeSettingBtn)
 
-        await action.waitForDisplayed(component.settingTitleText)
-        await assert.checkText(component.settingTitleText, 'Setting')
+        await action.click(component.settingNetworkManageBtn)
     })
 
-    // === Check Network Management Page ===
-    it('[Tap] Check Network Management Btn',async () => {
-        await action.waitForDisplayed(component.settingNetworkManageBtn)
+    it('[Display] Check X Btn', async () => {
+        // assert
+        await assert.checkElementDisplayed(component.networkmanagementXBtn)
+    })
 
-        await action.click(component.settingNetworkManageBtn)
+    it('[Display] Check Popular tab', async () => {
+        // assert
+        await assert.checkElementDisplayed(component.networkmanagementPopularTab)
+    })
 
-        await action.waitForDisplayed(component.networkmanagementTitleText)
-        await assert.checkText(component.networkmanagementTitleText, 'Add Network')
+    it('[Display] Check Custom Network tab', async () => {
+        // assert
+        await assert.checkElementDisplayed(component.networkmanagementCustomTab)
+    })
 
-        await action.waitForDisplayed(component.networkmanagementCustomTab)
-        await assert.checkText(component.networkmanagementCustomTab, 'CUSTOM NETWORK')
-
-        await action.waitForDisplayed(component.networkmanagementPopularTab)
-        await assert.checkText(component.networkmanagementPopularTab, 'POPULAR')
-
+    it('[Display] Check Add Custom Network Btn', async () => {
+        // action (to display Btn)
         await action.click(component.networkmanagementCustomTab)
 
-        await action.waitForDisplayed(component.networkmanagementAddcustomnetBtn)
+        // assert
+        await assert.checkElementDisplayed(component.customTabAddcustomnetBtn)
+
+        // after (back to Popular)
+        await action.click(component.networkmanagementPopularTab)
     })
-    it('[Tap] Check Add Custom Network Btn',async () => {
-        await action.waitForDisplayed(component.networkmanagementAddcustomnetBtn)
 
-        await action.click(component.networkmanagementAddcustomnetBtn)
+    it('[Wording] Check Title text', async () => {
+        // assert
+        await assert.checkText(component.networkmanagementTitleText, 'Add Network')
+    })
 
-        await action.waitForDisplayed(component.customnetTitleText)
+    it('[Wording] Check X Btn text', async () => {
+        // assert
+        await assert.checkText(component.networkmanagementXBtn, '')
+    })
+
+    it('[Wording] Check Popular tab text', async () => {
+        // assert
+        await assert.checkText(component.networkmanagementPopularTab, 'POPULAR')
+    })
+
+    it('[Wording] Check Custom Network tab text', async () => {
+        // assert
+        await assert.checkText(component.networkmanagementCustomTab, 'CUSTOM NETWORK')
+    })
+
+    it('[Wording] Check Add Custom Network Btn text', async () => {
+        // action (to display Btn)
+        await action.click(component.networkmanagementCustomTab)
+
+        // assert
+        await assert.checkText(component.customTabAddcustomnetBtn, 'Add Custom Network')
+
+        // after (back to Popular)
+        await action.click(component.networkmanagementPopularTab)
+    })
+
+    it('[Tap] Check X Btn tap', async () => {
+        // action
+        await action.click(component.networkmanagementXBtn)
+
+        // assert
+        await assert.checkElementDisplayed(component.settingTitleText)
+
+        // after (back to Network Management)
+        await action.click(component.settingNetworkManageBtn)
+    })
+
+    it('[Tap] Check Custom Network tab tap', async () => {
+        // action
+        await action.click(component.networkmanagementCustomTab)
+
+        // assert
+        await assert.checkElementDisplayed(component.customTabAddcustomnetBtn)
+    })
+
+    it('[Tap] Check Popular tab tap', async () => {
+        // action
+        await action.click(component.networkmanagementPopularTab)
+
+        // assert
+        await assert.checkElementDisplayed(component.popularTabBitkubChainTestnet)
+    })
+
+    it('[Tap] Check Add Custom Network Btn tap', async () => {
+        // action
+        await action.click(component.networkmanagementCustomTab)
+
+        await action.click(component.customTabAddcustomnetBtn)
+
+        // assert
         await assert.checkText(component.customnetTitleText, 'Custom Network')
     })
-    it('[Display] Check Error Message Text Field',async () => {
-        await action.waitForDisplayed(component.customnetNetworkNameTextField)
+})
 
+describe('Custom Network Page Test Scenario', () => {
+    it('[Display] Check Back Btn', async () => {
+        // assert
+        await assert.checkElementDisplayed(component.customnetBackBtn)
+    })
+
+    it('[Display] Check X Btn', async () => {
+        // assert
+        await assert.checkElementDisplayed(component.customnetXBtn)
+    })
+
+    it('[Display] Check Network Name Info Btn', async () => {
+        // assert
+        await assert.checkElementDisplayed(component.customnetNetworkNameInfoBtn)
+    })
+
+    it('[Display] Check Network Name TextField', async () => {
+        // assert
+        await assert.checkElementDisplayed(component.customnetNetworkNameTextField)
+    })
+
+    it('[Display] Check Network URL Info Btn', async () => {
+        // assert
+        await assert.checkElementDisplayed(component.customnetNetworkURLInfoBtn)
+    })
+
+    it('[Display] Check Network URL TextField', async () => {
+        // assert
+        await assert.checkElementDisplayed(component.customnetNetworkUrlTextField)
+    })
+
+    it('[Display] Check Chain ID Info Btn', async () => {
+        // assert
+        await assert.checkElementDisplayed(component.customnetChainIDInfoBtn)
+    })
+
+    it('[Display] Check Chain ID TextField', async () => {
+        // assert
+        await assert.checkElementDisplayed(component.customnetChainIDTextField)
+    })
+
+    it('[Display] Check Symbol Info Btn', async () => {
+        // assert
+        await assert.checkElementDisplayed(component.customnetSymbolInfoBtn)
+    })
+
+    it('[Display] Check Symbol TextField', async () => {
+        // assert
+        await assert.checkElementDisplayed(component.customnetSymbolTextField)
+    })
+
+    it('[Display] Check Block Explorer URL (Optional) Info Btn', async () => {
+        // assert
+        await assert.checkElementDisplayed(component.customnetBlockExplorInfoBtn)
+    })
+
+    it('[Display] Check Block Explorer URL (Optional) TextField', async () => {
+        // assert
+        await assert.checkElementDisplayed(component.customnetBlockExplorTextField)
+    })
+
+    it('[Display] Check Cancel Btn', async () => {
+        // assert
+        await assert.checkElementDisplayed(component.customnetCancelBtn)
+    })
+
+    it('[Display] Check Confirm Btn', async () => {
+        // assert
+        await assert.checkElementDisplayed(component.customnetConfirmBtn)
+    })
+
+    it('[Display] Check Confirm Btn should be disable', async () => {
+        // assert
+        await assert.checkDisabled(component.customnetConfirmBtn)
+    })
+
+    it('[Wording] Check Back Btn text', async () => {
+        // assert
+        await assert.checkText(component.customnetBackBtn, '')
+    })
+
+    it('[Wording] Check X Btn text', async () => {
+        // assert
+        await assert.checkText(component.customnetXBtn, '')
+    })
+
+    it('[Wording] Check Title text', async () => {
+        // assert
+        await assert.checkText(component.customnetTitleText, 'Custom Network')
+    })
+
+    it('[Wording] Check Warning text', async () => {
+        // assert
+        await assert.checkText(component.customnetWarningText, 'A Malicious network provider can lie about the state of the blockchain and record your network activity. Only add networks you trust.')
+    })
+
+    it('[Wording] Check Network Name Info Btn text', async () => {
+        // assert
+        await assert.checkText(component.customnetNetworkNameInfoBtn, '')
+    })
+
+    it('[Wording] Check Network Name text', async () => {
+        // assert
+        await assert.checkText(component.customnetNetworkNameText, 'Network Name')
+    })
+
+    it('[Wording] Check Network URL Info Btn text', async () => {
+        // assert
+        await assert.checkText(component.customnetNetworkURLInfoBtn, '')
+    })
+
+    it('[Wording] Check Network URL text', async () => {
+        // assert
+        await assert.checkText(component.customnetNetworkURLText, 'Network URL')
+    })
+
+    it('[Wording] Check Chain ID Info Btn text', async () => {
+        // assert
+        await assert.checkText(component.customnetChainIDInfoBtn, '')
+    })
+
+    it('[Wording] Check Chain ID text', async () => {
+        // assert
+        await assert.checkText(component.customnetChainIDText, 'Chain ID')
+    })
+
+    it('[Wording] Check Symbol Info Btn text', async () => {
+        // assert
+        await assert.checkText(component.customnetSymbolInfoBtn, '')
+    })
+
+    it('[Wording] Check Symbol text', async () => {
+        // assert
+        await assert.checkText(component.customnetSymbolText, 'Symbol')
+    })
+
+    it('[Wording] Check Block Explorer URL (Optional) Info Btn text', async () => {
+        // assert
+        await assert.checkText(component.customnetBlockExplorInfoBtn, '')
+    })
+
+    it('[Wording] Check Block Explorer URL (Optional) text', async () => {
+        // assert
+        await assert.checkText(component.customnetBlockExplorText, 'Block Explorer URL (Optional)')
+    })
+
+    it('[Wording] Check Cancel Btn text', async () => {
+        // assert
+        await assert.checkText(component.customnetCancelBtn, 'Cancel')
+    })
+
+    it('[Wording] Check Confirm Btn text', async () => {
+        // assert
+        await assert.checkText(component.customnetConfirmBtn, 'Confirm')
+    })
+
+    it('[Tap] Check Back Btn tap', async () => {
+        // action
+        await action.click(component.customnetBackBtn)
+
+        // assert
+        await assert.checkElementDisplayed(component.networkmanagementTitleText)
+
+        // after (back to Custom Network)
+        await action.click(component.customTabAddcustomnetBtn)
+    })
+
+    it('[Tap] Check X Btn tap', async () => {
+        // action
+        await action.click(component.customnetXBtn)
+
+        // assert
+        await assert.checkElementDisplayed(component.networkmanagementTitleText)
+
+        // after (back to Custom Network)
+        await action.click(component.customTabAddcustomnetBtn)
+    })
+
+    it('[Tap] Check Network Name Info Btn tap', async () => {
+        // action
+        await action.click(component.customnetNetworkNameInfoBtn)
+
+        // assert
+        await assert.checkText(component.customnetNetworkNameInfoText, 'The name associated with this network.')
+        await action.pause(1000)
+
+        // after (tap out of pop-up)
+        await action.Tap(1000, 2800)
+    })
+
+    it('[Tap] Check Network URL Info Btn tap', async () => {
+        // action
+        await action.click(component.customnetNetworkURLInfoBtn)
+
+        // assert
+        await assert.checkText(component.customnetNetworkURLInfoText, 'The URL used to access this network.')
+        await action.pause(1000)
+
+        // after (tap out of pop-up)
+        await action.Tap(1000, 2800)
+    })
+
+    it('[Tap] Check Chain ID Info Btn tap', async () => {
+        // action
+        await action.click(component.customnetChainIDInfoBtn)
+
+        // assert
+        await assert.checkText(component.customnetChainIDInfoText, 'The chain ID used to sign transactions for this network.')
+        await action.pause(1000)
+
+        // after (tap out of pop-up)
+        await action.Tap(1000, 2800)
+    })
+
+    it('[Tap] Check Symbol Info Btn tap', async () => {
+        // action
+        await action.click(component.customnetSymbolInfoBtn)
+
+        // assert
+        await assert.checkText(component.customnetSymbolInfoText, 'The ticker symbol displayed for this network’s currency.')
+        await action.pause(1000)
+
+        // after (tap out of pop-up)
+        await action.Tap(1000, 2800)
+    })
+
+    it('[Tap] Check Block Explorer URL (Optional) Info Btn tap', async () => {
+        // action
+        await action.click(component.customnetBlockExplorInfoBtn)
+
+        // assert
+        await assert.checkText(component.customnetBlockExplorInfoText, 'The URL used as the block explorer for this network.')
+        await action.pause(1000)
+
+        // after (tap out of pop-up)
+        await action.Tap(1000, 2800)
+    })
+
+    it('[Tap] Check Cancel Btn tap', async () => {
+        // action
+        await action.click(component.customnetCancelBtn)
+
+        // assert
+        await assert.checkElementDisplayed(component.networkmanagementTitleText)
+
+        // after (back to Custom Network)
+        await action.click(component.customTabAddcustomnetBtn)
+    })
+
+    it('[Validation] Check Error Message Text Field', async () => {
+        // action
         await action.click(component.customnetNetworkNameTextField)
 
-        // Press 'Done' on android keyboard
+        // Press 'Done' on android keyboard device
         await action.Tap(1335, 2831)
-
-        await action.waitForDisplayed(component.customnetNetworkNameRequired)
-        await assert.checkText(component.customnetNetworkNameRequired, 'Network Name is required')
-
-        await action.waitForDisplayed(component.customnetSymbolTextField)
 
         await action.click(component.customnetSymbolTextField)
 
-        // Press 'Done' on android keyboard
+        // Press 'Done' on android keyboard device
         await action.Tap(1335, 2831)
 
-        await action.waitForDisplayed(component.customnetSymbolRequired)
+        // assert
+        await assert.checkText(component.customnetNetworkNameRequired, 'Network Name is required')
         await assert.checkText(component.customnetSymbolRequired, 'Symbol is required')
     })
-    it('[Tap] Check Add Custom Network Success',async () => {
-        await action.waitForDisplayed(component.customnetNetworkUrlTextField)
 
+    it('[Function] Check Add Custom Network Success', async () => {
+        // action
         await action.enterText(component.customnetNetworkUrlTextField, `${process.env.TEST_URL}`)
+        await action.pause(2000)
 
-        await action.waitForDisplayed(component.customnetNetworkNameTextField)
+        // assert
         await assert.checkText(component.customnetNetworkNameTextField, 'goerli')
 
+        // action
         await action.click(component.customnetConfirmBtn)
-
-        await action.waitForDisplayed(component.addnetsuccessSwitchgoerliBtn)
 
         await action.click(component.addnetsuccessSwitchgoerliBtn)
 
-        await action.waitForDisplayed(component.homeBtn)
-
-        await action.waitForDisplayed(component.homeGoerlichain)
+        // assert
         await assert.checkText(component.homeGoerlichain, 'goerli')
+        await assert.checkText(component.homeETHToken, 'ETH')
 
-        // Go back to Setting page
+        // after (back to Network Management)
         await action.click(component.homeSettingBtn)
+        await action.click(component.settingNetworkManageBtn)
+    })
+})
+
+describe('Confirm Remove Chain Test Scenario', () => {
+    beforeAll(async () => {
+        await action.click(component.networkmanagementCustomTab)
+
+        await action.click(component.customTabGoerliRemoveBtn)
     })
 
-    // // === Check Network Management Page (Remove) ===
-    // it('[Tap] Check Remove Custom Network Btn',async () => {
-    //     await action.waitForDisplayed(component.settingNetworkManageBtn)
-        
-    //     await action.click(component.settingNetworkManageBtn)
+    it('[Display] Check Cancel Btn', async () => {
+        // assert
+        await assert.checkElementDisplayed(component.confirmRemoveCancelBtn)
+    })
 
-    //     await action.waitForDisplayed(component.networkmanagementCustomTab)
+    it('[Display] Check Confirm Btn', async () => {
+        // assert
+        await assert.checkElementDisplayed(component.confirmRemoveRemoveBtn)
+    })
 
-    //     await action.click(component.networkmanagementCustomTab)
+    it('[Wording] Check Title Text', async () => {
+        // assert
+        await assert.checkText(component.confirmRemoveTitleText, 'Remove goerli Chain')
+    })
 
-    //     await action.waitForDisplayed(component.networkmanagementRemoveCustomnetBtn)
-    //     await assert.checkElemenDisplayed(component.networkmanagementRemoveCustomnetBtn)
+    it('[Wording] Check Text', async () => {
+        // assert
+        await assert.checkText(component.confirmRemoveText, 'Assets on removed networks will no longer be displayed.')
+    })
 
-    //     await action.click(component.networkmanagementRemoveCustomnetBtn)
+    it('[Wording] Check Remove Btn text', async () => {
+        // assert
+        await assert.checkText(component.confirmRemoveRemoveBtn, 'Remove')
+    })
 
-    //     await action.waitForDisplayed(component.confirmremoveBtn)
-    //     await assert.checkText(component.confirmremoveBtn, 'Remove')
+    it('[Wording] Check Cancel Btn text', async () => {
+        // assert
+        await assert.checkText(component.confirmRemoveCancelBtn, 'Cancel')
+    })
 
-    //     await action.click(component.confirmremoveBtn)
+    it('[Tap] Check Cancel Btn tap', async () => {
+        // action
+        await action.click(component.confirmRemoveCancelBtn)
 
-    //     await action.waitForDisplayed(component.networkmanagementCloseIconBtn)
+        // assert
+        await assert.checkElementDisplayed(component.networkmanagementTitleText)
 
-    //     await action.click(component.networkmanagementCloseIconBtn)
-    // })
+        // after (back to Confirm Remove pop-up)
+        await action.click(component.customTabGoerliRemoveBtn)
+    })
+
+    it('[Tap] Check Remove Btn', async () => {
+        // action
+        await action.click(component.confirmRemoveRemoveBtn)
+
+        // action (should not exist)
+        await action.waitForExist(component.customTabGoerliRemoveBtn, 3000, true)
+    })
 
     afterAll(async () => {
-        await action.closeApps(`${process.env.PACKAGE_ID}`)
+        await action.removeApps(`${process.env.PACKAGE_ID}`)
     })
 })
