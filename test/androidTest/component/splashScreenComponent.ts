ccd74aba4e5b26377ab6ae64777de99ea7994b87
Juggrid <champ_psu@outlook.co.th>
2024-01-29 11:00:47 +0700

    set default id format

--- test/androidTest/component/splashScreenComponent.ts
+++ test/androidTest/component/splashScreenComponent.ts
@@ -1,29 +1,32 @@
 export class splashScreenComponent {
-    
     //  Splash Screen Component
-    splashScreenHeaderText = `android=new UiSelector().resourceId("guideline-title-1")`
-    splashScreenDescriptionText = `android=new UiSelector().resourceId("guideline-description-1")`
-
-    splashScreenHeaderText2 = `android=new UiSelector().resourceId("guideline-title-2")`
-    splashScreenDescriptionText2 = `android=new UiSelector().resourceId("guideline-description-2")`
+    splashScreenHeaderText = this.id('guideline-title-1')
+    splashScreenDescriptionText = this.id('guideline-description-1')
     
-    splashScreenHeaderText3 = `android=new UiSelector().resourceId("guideline-title-3")`
-    splashScreenDescriptionText3 = `android=new UiSelector().resourceId("guideline-description-3")`
-
-    splashScreenCreateWalletBtn = `android=new UiSelector().resourceId("guideline-createwallet-btn")`
+    splashScreenHeaderText2 = this.id('guideline-title-2')
+    splashScreenDescriptionText2 = this.id('guideline-description-2')
+    
+    splashScreenHeaderText3 = this.id('guideline-title-3')
+    splashScreenDescriptionText3 = this.id('guideline-description-3')
+    
+    splashScreenCreateWalletBtn = this.id('guideline-createwallet-btn')
     splashScreenCreateWalletBtnText = `android=new UiSelector().text("Create a new wallet").className("android.widget.TextView")`
     
-    splashScreenImportWalletBtn = `android=new UiSelector().resourceId("guideline-importwallet-btn")`
+    splashScreenImportWalletBtn = this.id('guideline-importwallet-btn')
+    
     splashScreenImportWalletBtnText = `android=new UiSelector().text("Import a wallet").className("android.widget.TextView")`
-
+    
     // Create Wallet Component
     createWalletTitleText = `android=new UiSelector().text("Create Wallet").className("android.view.View")`
-    createWalletBackBtn = `android=new UiSelector().text("").className("android.widget.TextView")`
+    createWalletBackBtn = this.id('createwallet-header-backbtn')
     
     // Import Wallet Component
     importWalletTitleText = `android=new UiSelector().text("Import Wallet").className("android.view.View")`
     importWalletBackBtn = `android=new UiSelector().text("").className("android.widget.TextView")`
     
+    private id (id: string){
+        return (`android=new UiSelector().resourceId("${id}")`)
+    }
 }
 
 
