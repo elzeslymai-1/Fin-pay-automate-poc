7c02da0a2c647865c6371d0dda348e8a8b767f89
Juggrid <champ_psu@outlook.co.th>
2024-01-30 09:48:22 +0700

    Resolve Wording and Variable name

--- test/androidTest/component/customTokenComponent.ts
+++ test/androidTest/component/customTokenComponent.ts
@@ -11,36 +11,36 @@ export class customTokenComponent {
     successContinueBtn = `android=new UiSelector().text("Continue").className("android.widget.TextView")`
 
     // Custom Token Page Component
-    customtokenTitleText = `android=new UiSelector().text("Custom token").className("android.view.View")`
-    customtokenTokenHeaderText = `android=new UiSelector().text("Token").className("android.widget.TextView")`
-    customtokenCustomTokenHeaderText = `android=new UiSelector().text("Custom Token").className("android.widget.TextView")`
+    customTokenTitleText = `android=new UiSelector().text("Custom token").className("android.view.View")`
+    customTokenTokenHeaderText = `android=new UiSelector().text("Token").className("android.widget.TextView")`
+    customTokenCustomTokenHeaderText = `android=new UiSelector().text("Custom Token").className("android.widget.TextView")`
     
-    customtokenBackBtn = `android=new UiSelector().text("").className("android.widget.TextView")`
-    customtokenNetworkBtn = `android=new UiSelector().resourceId("home-chainselect-btn")`
-    customtokenSearchTextField = `android=new UiSelector().resourceId("home-walletmodal-search")`
-    customtokenRemoveBtn = `android=new UiSelector().text("").className("android.widget.TextView")`
+    customTokenBackBtn = `android=new UiSelector().text("").className("android.widget.TextView")`
+    customTokenNetworkBtn = `android=new UiSelector().resourceId("home-chainselect-btn")`
+    customTokenSearchTextField = `android=new UiSelector().resourceId("home-walletmodal-search")`
+    customTokenRemoveBtn = `android=new UiSelector().text("").className("android.widget.TextView")`
 
-    customtokenNetworkGoerliBtn = `android=new UiSelector().text("G").className("android.widget.TextView")`
+    customTokenNetworkGoerliBtn = `android=new UiSelector().text("G").className("android.widget.TextView")`
 
-    customtokenKubToken = `android=new UiSelector().text("KUB").className("android.widget.TextView")`
-    customtokenWToken = `android=new UiSelector().text("WToken").className("android.widget.TextView")`
-    customtokenGoerliToken = `android=new UiSelector().text("Goerli").className("android.widget.TextView")`
-    customtokenFintestToken = `android=new UiSelector().text("FinTestToken").className("android.widget.TextView")`
+    customTokenKubToken = `android=new UiSelector().text("KUB").className("android.widget.TextView")`
+    customTokenWToken = `android=new UiSelector().text("WToken").className("android.widget.TextView")`
+    customTokenGoerliToken = `android=new UiSelector().text("Goerli").className("android.widget.TextView")`
+    customTokenFinTestToken = `android=new UiSelector().text("FinTestToken").className("android.widget.TextView")`
 
-    customtokenAddcustomtokenBtn = `android=new UiSelector().text("Add Custom Token").className("android.widget.TextView")`
-    customtokenTokenAddressTextField = `android=new UiSelector().text("Token address (0x...)").className("android.widget.EditText")`
-    customtokenAddFintoken = `android=new UiSelector().text("FinTestToken").className("android.widget.TextView")`
-    customtokenNoresultText = `android=new UiSelector().text("No Result").className("android.widget.TextView")`
-    customtokenCheckbox = `android=new UiSelector().className("android.widget.CheckBox")`
-    customtokenConfirmBtn = `android=new UiSelector().text("Confirm").className("android.widget.TextView")`
+    customTokenAddCustomTokenBtn = `android=new UiSelector().text("Add Custom Token").className("android.widget.TextView")`
+    customTokenTokenAddressTextField = `android=new UiSelector().text("Token address (0x...)").className("android.widget.EditText")`
+    customTokenAddFinToken = `android=new UiSelector().text("FinTestToken").className("android.widget.TextView")`
+    customTokenNoResultText = `android=new UiSelector().text("No Result").className("android.widget.TextView")`
+    customTokenCheckbox = `android=new UiSelector().className("android.widget.CheckBox")`
+    customTokenConfirmBtn = `android=new UiSelector().text("Confirm").className("android.widget.TextView")`
 
     // Network Component
     networkTitleText = `android=new UiSelector().text("Select Network").className("android.widget.TextView")`
     networkBitkubChain = `android=new UiSelector().text("Bitkub Chain Testnet").resourceId("bottomsheet-chain-name")`
     networkGoerliChain = `android=new UiSelector().text("goerli").resourceId("bottomsheet-chain-name")`
-    networkAddnetworkBtn = `android=new UiSelector().text("Add Network").className("android.widget.TextView")`
+    networkAddNetworkBtn = `android=new UiSelector().text("Add Network").className("android.widget.TextView")`
 
-    addnetworkCustomTab = `android=new UiSelector().text("CUSTOM NETWORK").className("android.widget.TextView")`
+    addNetworkCustomTab = `android=new UiSelector().text("CUSTOM NETWORK").className("android.widget.TextView")`
     customTabAddNetwork = `android=new UiSelector().text("Add Custom Network").className("android.widget.TextView")`
     customNetworkURLTextField = `android=new UiSelector().className("android.widget.EditText").index(7)`
     customNetworkConfirmBtn = `android=new UiSelector().text("Confirm").className("android.widget.TextView")`
--- test/androidTest/specs/customTokenPage.e2e.ts
+++ test/androidTest/specs/customTokenPage.e2e.ts
@@ -6,7 +6,7 @@ const component = new customTokenComponent()
 const action = new Action()
 const assert = new Assertion()
 
-describe('Custom Token Page Test Senario', () => {
+describe('Custom Token Page Test Scenario', () => {
 
     beforeAll(async () => {
         await action.installApps(`${process.env.PATH_APK}`)
@@ -28,52 +28,52 @@ describe('Custom Token Page Test Senario', () => {
 
     it('[Display] => Check Back Btn',async () => {
         // assert
-        await assert.checkElementDisplayed(component.customtokenBackBtn)
+        await assert.checkElementDisplayed(component.customTokenBackBtn)
     })
 
     it('[Display] => Check Network Btn',async () => {
         // assert
-        await assert.checkElementDisplayed(component.customtokenNetworkBtn)
+        await assert.checkElementDisplayed(component.customTokenNetworkBtn)
     })
 
     it('[Display] => Check Search TextField',async () => {
         // assert
-        await assert.checkElementDisplayed(component.customtokenSearchTextField)
+        await assert.checkElementDisplayed(component.customTokenSearchTextField)
     })
 
     it('[Display] => Check Add Custom Token Btn',async () => {
         // assert
-        await assert.checkElementDisplayed(component.customtokenAddcustomtokenBtn)
+        await assert.checkElementDisplayed(component.customTokenAddCustomTokenBtn)
     })
 
     it('[Wording] => Check Title Text',async () => {
         // assert
-        await assert.checkText(component.customtokenTitleText, 'Custom token')
+        await assert.checkText(component.customTokenTitleText, 'Custom token')
     })
 
     it('[Wording] => Check Search Placeholder Text',async () => {
         // assert
-        await assert.checkText(component.customtokenSearchTextField, 'Search...')
+        await assert.checkText(component.customTokenSearchTextField, 'Search...')
     })
 
     it('[Wording] => Check Token Header Text',async () => {
         // assert
-        await assert.checkText(component.customtokenTokenHeaderText, 'Token')
+        await assert.checkText(component.customTokenTokenHeaderText, 'Token')
     })
 
     it('[Wording] => Check Custom Token Header Text',async () => {
         // assert
-        await assert.checkText(component.customtokenCustomTokenHeaderText, 'Custom Token')
+        await assert.checkText(component.customTokenCustomTokenHeaderText, 'Custom Token')
     })
 
     it('[Wording] => Check Add Custom Token Btn Text',async () => {
         // assert
-        await assert.checkText(component.customtokenAddcustomtokenBtn, 'Add Custom Token')
+        await assert.checkText(component.customTokenAddCustomTokenBtn, 'Add Custom Token')
     })
 
     it('[Tap] => Check Back Btn Tap',async () => {
         // action
-        await action.click(component.customtokenBackBtn)
+        await action.click(component.customTokenBackBtn)
 
         // assert
         await assert.checkElementDisplayed(component.settingCustomTokenBtn)
@@ -84,15 +84,15 @@ describe('Custom Token Page Test Senario', () => {
 
     it('[Function] => Check Change Network Btn Tap',async () => {
         // action
-        await action.click(component.customtokenNetworkBtn)
+        await action.click(component.customTokenNetworkBtn)
         
         // assert
         await assert.checkText(component.networkTitleText, 'Select Network')
 
         // action (for add network goerli)
-        await action.click(component.networkAddnetworkBtn)
+        await action.click(component.networkAddNetworkBtn)
 
-        await action.click(component.addnetworkCustomTab)
+        await action.click(component.addNetworkCustomTab)
 
         await action.click(component.customTabAddNetwork)
 
@@ -107,75 +107,75 @@ describe('Custom Token Page Test Senario', () => {
         await action.click(component.settingCustomTokenBtn)
 
         // assert
-        await assert.checkElementDisplayed(component.customtokenGoerliToken)
+        await assert.checkElementDisplayed(component.customTokenGoerliToken)
 
         // action
-        await action.click(component.customtokenNetworkGoerliBtn)
+        await action.click(component.customTokenNetworkGoerliBtn)
 
         await action.click(component.networkBitkubChain)
 
         // assert
-        await assert.checkElementDisplayed(component.customtokenKubToken)
+        await assert.checkElementDisplayed(component.customTokenKubToken)
     })
 
     it('[Search] => Check Add Custom Token no result',async () => {
         // action
-        await action.click(component.customtokenAddcustomtokenBtn)
+        await action.click(component.customTokenAddCustomTokenBtn)
 
-        await action.enterText(component.customtokenTokenAddressTextField, 'tokenaddress')
+        await action.enterText(component.customTokenTokenAddressTextField, 'tokenaddress')
         await action.pause(10000)
 
         // assert
-        await assert.checkElementDisplayed(component.customtokenNoresultText)
+        await assert.checkElementDisplayed(component.customTokenNoResultText)
 
         // after (back to Custom token)
-        await action.click(component.customtokenBackBtn)
+        await action.click(component.customTokenBackBtn)
     })
     
     it('[Function] => Check Add Custom Token Btn',async () => {
         // action
-        await action.click(component.customtokenNetworkBtn)
+        await action.click(component.customTokenNetworkBtn)
 
         await action.click(component.networkGoerliChain)
         
-        await action.click(component.customtokenAddcustomtokenBtn)
+        await action.click(component.customTokenAddCustomTokenBtn)
 
-        await action.enterText(component.customtokenTokenAddressTextField, `${process.env.TEST_ADDTOKEN}`)
+        await action.enterText(component.customTokenTokenAddressTextField, `${process.env.TEST_ADDTOKEN}`)
 
         // assert
-        await assert.checkText(component.customtokenAddFintoken, 'FinTestToken')
+        await assert.checkText(component.customTokenAddFinToken, 'FinTestToken')
 
         // action
-        await action.click(component.customtokenCheckbox)
+        await action.click(component.customTokenCheckbox)
 
-        await action.click(component.customtokenConfirmBtn)
+        await action.click(component.customTokenConfirmBtn)
 
         // assert
-        await assert.checkElementDisplayed(component.customtokenFintestToken)
+        await assert.checkElementDisplayed(component.customTokenFinTestToken)
     })
 
     it('[Function] => Check Remove Custom Token Btn',async () => {
         // action
-        await action.click(component.customtokenRemoveBtn)
+        await action.click(component.customTokenRemoveBtn)
 
         // assert
-        await assert.checkElementNotDisplayed(component.customtokenFintestToken)
+        await assert.checkElementNotDisplayed(component.customTokenFinTestToken)
 
         // after (change network to BitKubTestnet)
-        await action.click(component.customtokenNetworkGoerliBtn)
+        await action.click(component.customTokenNetworkGoerliBtn)
 
         await action.click(component.networkBitkubChain)
     })
 
     it('[Search] => Check Search TextField',async () => {
         //action        
-        await action.enterText(component.customtokenSearchTextField, 'kub')
+        await action.enterText(component.customTokenSearchTextField, 'kub')
 
         // assert
-        await assert.checkText(component.customtokenKubToken, 'KUB')
-        await assert.checkElementNotDisplayed(component.customtokenWToken)
+        await assert.checkText(component.customTokenKubToken, 'KUB')
+        await assert.checkElementNotDisplayed(component.customTokenWToken)
 
-        await action.clearText(component.customtokenSearchTextField)
+        await action.clearText(component.customTokenSearchTextField)
     })
 
     afterAll(async () => {
