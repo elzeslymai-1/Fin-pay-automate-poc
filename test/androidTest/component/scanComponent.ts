4f34496ca7801ff6d80e89c83df199b9cdf49617
Arm-mu <karis.arm13@gmail.com>
2024-01-30 13:32:01 +0700

    Refactor: scan android

--- test/androidTest/component/scanComponent.ts
+++ test/androidTest/component/scanComponent.ts
@@ -7,9 +7,13 @@ export class scanComponent {
     createWalletSuccessContinueBtn = `android=new UiSelector().resourceId("createwallet-success-continue")`
     homeScanBtn = `android=new UiSelector().resourceId("home-actiondock-scan")`
 
-    // Scan Component
-    scanPermissionBtn = `android=new UiSelector().resourceId("com.android.permissioncontroller:id/permission_allow_foreground_only_button")`
+    // Camera Permission Component
+    camPermissionText = `android=new UiSelector().resourceId("com.android.permissioncontroller:id/permission_message")`
+    camPermissionWhileAppBtn = `android=new UiSelector().resourceId("com.android.permissioncontroller:id/permission_allow_foreground_only_button")`
+    camPermissionOnlyBtn = `android=new UiSelector().resourceId("com.android.permissioncontroller:id/permission_allow_one_time_button")`
+    camPermissionDenyBtn = `android=new UiSelector().resourceId("com.android.permissioncontroller:id/permission_deny_button")`
 
+    // Scan Component
     scanBackBtn = `android=new UiSelector().text("").className("android.widget.TextView")`
     scanTitleText = `android=new UiSelector().text("Scan QR code").className("android.view.View")`
     scanReceiveBtn = `android=new UiSelector().text("").className("android.widget.TextView")`
@@ -18,16 +22,32 @@ export class scanComponent {
     scanUploadText = `android=new UiSelector().text("Upload QR code").className("android.widget.TextView")`
     scanBottomText = `android=new UiSelector().text("Align QR code within frame to scan").className("android.widget.TextView")`
 
+    // Deny Camera Permission Component
+    denyCamTitleText = `android=new UiSelector().text("Access Your Camera").className("android.widget.TextView")`
+    denyCamDescText = `android=new UiSelector().text("Please allow the app to access your camera in your privacy settings.").className("android.widget.TextView")`
+    denyCamOKBtn = `android=new UiSelector().text("OK").className("android.widget.TextView")`
+
     // Select Token Component
     selectTokenTitleText = `android=new UiSelector().text("Select Token").className("android.view.View")`
     selectTokenBackBtn = `android=new UiSelector().text("").className("android.widget.TextView")`
+    selectTokenKUB = `android=new UiSelector().text("KUB").className("android.widget.TextView")`
+    receiveDownloadBtn = `android=new UiSelector().text("").className("android.widget.TextView")`
+    receiveBackBtn = `android=new UiSelector().text("").className("android.widget.TextView")`
+
+    // Photos Permission Component
+    photoPermissionAllowBtn1 = `android=new UiSelector().resourceId("com.android.permissioncontroller:id/permission_allow_button")`
+    photoPermissionDenyBtn1 = `android=new UiSelector().resourceId("com.android.permissioncontroller:id/permission_deny_button")`
+    photoPermissionAllowBtn2 = `android=new UiSelector().resourceId("com.android.permissioncontroller:id/permission_allow_all_button")`
+    photoPermissionDenyBtn2 = `android=new UiSelector().resourceId("com.android.permissioncontroller:id/permission_deny_button")`
 
     // Upload Component
-    uploadPermissionBtn1 = `android=new UiSelector().resourceId("com.android.permissioncontroller:id/permission_allow_button")`
-    uploadPermissionBtn2 = `android=new UiSelector().resourceId("com.android.permissioncontroller:id/permission_allow_all_button")`
-
     uploadPopupText = `android=new UiSelector().resourceId("com.google.android.providers.media.module:id/privacy_text")`
     uploadPopupPhotoSelected = `android=new UiSelector().resourceId("com.google.android.providers.media.module:id/icon_thumbnail")`
+
+    // Deny Photo Permission Component
+    denyPhotoTitleText = `android=new UiSelector().text("Access Your Photos").className("android.widget.TextView")`
+    denyPhotoDescText = `android=new UiSelector().text("Please allow the app to access your photo album in your privacy settings.").className("android.widget.TextView")`
+    denyPhotoOKBtn = `android=new UiSelector().text("OK").className("android.widget.TextView")`
 
     // XPath
     uploadPopupCloseBtn = `//android.widget.ImageButton[@content-desc="Cancel"]`
--- test/androidTest/specs/scanPage.e2e.ts
+++ test/androidTest/specs/scanPage.e2e.ts
@@ -6,117 +6,198 @@ const component = new scanComponent()
 const action = new Action()
 const assert = new Assertion()
 
-describe('Scan QR code Test Scenario',() => {
-    beforeAll(async () => {
-        await action.click(component.splashScreenCreateWalletBtn)
-        
-        await action.click(component.createWallet12wordBtn)
-        await action.click(component.createWalletContinueBtn)
-        await action.pause(5000)
+describe('Deny Camara Permission Test Scenario', () => {
+   beforeAll(async () => {
+      await action.click(component.splashScreenCreateWalletBtn)
 
-        await action.click(component.createWalletSuccessContinueBtn)
+      await action.click(component.createWallet12wordBtn)
+      await action.click(component.createWalletContinueBtn)
+      await action.pause(5000)
 
-        await action.click(component.homeScanBtn)
+      await action.click(component.createWalletSuccessContinueBtn)
 
-        // action for handle permission
-        await action.click(component.scanPermissionBtn)
-    })
+      await action.click(component.homeScanBtn)
 
-    it('[Display] => Check Back Btn',async () => {
-       // assert
-       await assert.checkElementDisplayed(component.scanBackBtn) 
-    })
+      await action.click(component.camPermissionDenyBtn)
+   })
 
-    it('[Display] => Check Receive Btn',async () => {
-        // assert
-        await assert.checkElementDisplayed(component.scanReceiveBtn) 
-     })
+   it('[Display] => Check OK Btn',async () => {
+      // assert
+      await assert.checkElementDisplayed(component.denyCamOKBtn)
+   })
+   
+   it('[Wording] => Check Title Text',async () => {
+      // assert
+      await assert.checkText(component.denyCamTitleText, 'Access Your Camera')
+   })
 
-     it('[Display] => Check Upload QR code Btn',async () => {
-        // assert
-        await assert.checkElementDisplayed(component.scanUploadBtn) 
-     })
+   it('[Wording] => Check Description Text',async () => {
+      // assert
+      await assert.checkText(component.denyCamDescText, 'Please allow the app to access your camera in your privacy settings.')
+   })
 
-     it('[Wording] => Check Back Btn Text',async () => {
-        // assert
-        await assert.checkText(component.scanBackBtn, '') 
-     })
+   it('[Wording] => Check OK Btn Text',async () => {
+      // assert
+      await assert.checkText(component.denyCamOKBtn, 'OK')
+   })
 
-     it('[Wording] => Check Title Text',async () => {
-        // assert
-        await assert.checkText(component.scanTitleText, 'Scan QR code') 
-     })
+   it('[Tap] => Check OK Btn Tap',async () => {
+      // action
+      await action.click(component.denyCamOKBtn)
 
-     it('[Wording] => Check Receive Btn Text',async () => {
-        // assert
-        await assert.checkText(component.scanReceiveText, 'Receive') 
-     })
+      // assert
+      await assert.checkElementDisplayed(component.homeScanBtn)
+   })
+})
 
-     it('[Wording] => Check Upload QR code Btn Text',async () => {
-        // assert
-        await assert.checkText(component.scanUploadText, 'Upload QR code') 
-     })
+describe('Deny Photos Permission Test Scenario', () => {
+   beforeAll(async () => {
+      await action.click(component.homeScanBtn)
+      await action.click(component.camPermissionWhileAppBtn)
 
-     it('[Wording] => Check Bottom Text',async () => {
-        // assert
-        await assert.checkText(component.scanBottomText, 'Align QR code within frame to scan') 
-     })
+      await action.click(component.scanUploadBtn)
 
-     it('[Tap] => Check Back Btn Tap',async () => {
-        // action
-        await action.click(component.scanBackBtn)
+      await action.click(component.photoPermissionDenyBtn1)
+      
+      await action.click(component.photoPermissionDenyBtn2)
+   })
 
-        // assert
-        await assert.checkElementDisplayed(component.homeScanBtn)
+   it('[Display] => Check OK Btn',async () => {
+      // assert
+      await assert.checkElementDisplayed(component.denyPhotoOKBtn)
+   })
 
-        // after (back to Scan)
-        await action.click(component.homeScanBtn)
-     })
+   it('[Wording] => Check Title Text',async () => {
+      // assert
+      await assert.checkText(component.denyPhotoTitleText, 'Access Your Photos')
+   })
 
-     it('[Tap] => Check Receive Btn Tap',async () => {
-        // action
-        await action.click(component.scanReceiveBtn)
+   it('[Wording] => Check Description Text',async () => {
+      // assert
+      await assert.checkText(component.denyPhotoDescText, 'Please allow the app to access your photo album in your privacy settings.')
+   })
 
-        // assert
-        await assert.checkElementDisplayed(component.selectTokenTitleText)
+   it('[Wording] => Check OK Btn Text',async () => {
+      // assert
+      await assert.checkText(component.denyPhotoOKBtn, 'OK')
+   })
 
-        // after (back to Scan)
-        await action.click(component.selectTokenBackBtn)
-     })
+   it('[Tap] => Check OK Btn Tap',async () => {
+      // action
+      await action.click(component.denyPhotoOKBtn)
 
-     it('[Tap] => Check Upload QR code Btn Tap',async () => {
-        // action
-        await action.click(component.scanUploadBtn)
-    
-        // action (for handle permission)
-        await action.click(component.uploadPermissionBtn1)
-        await action.click(component.uploadPermissionBtn2)
-        await action.pause(2000)
+      // assert
+      await assert.checkElementDisplayed(component.scanUploadBtn)
+   })
+})
 
-        // assert
-        await assert.checkElementDisplayed(component.uploadPopupText)
+describe('Scan QR code Test Scenario', () => {
+   it('[Display] => Check Back Btn', async () => {
+      // assert
+      await assert.checkElementDisplayed(component.scanBackBtn)
+   })
 
-        // after (back to Scan)
-        await action.click(component.uploadPopupCloseBtn)
-     })
+   it('[Display] => Check Receive Btn', async () => {
+      // assert
+      await assert.checkElementDisplayed(component.scanReceiveBtn)
+   })
 
-     it('[Function] => Selected upload photo',async () => {
-        // action
-        await action.click(component.scanUploadBtn)
-        await action.pause(2000)
+   it('[Display] => Check Upload QR code Btn', async () => {
+      // assert
+      await assert.checkElementDisplayed(component.scanUploadBtn)
+   })
 
-        await action.click(component.uploadPopupPhotoSelected)
+   it('[Wording] => Check Back Btn Text', async () => {
+      // assert
+      await assert.checkText(component.scanBackBtn, '')
+   })
 
-        // assert
-        await assert.checkElementDisplayed(component.sendTitleText)
+   it('[Wording] => Check Title Text', async () => {
+      // assert
+      await assert.checkText(component.scanTitleText, 'Scan QR code')
+   })
 
-        // after (back to Scan)
-        await action.click(component.sendBackBtn)
+   it('[Wording] => Check Receive Btn Text', async () => {
+      // assert
+      await assert.checkText(component.scanReceiveText, 'Receive')
+   })
 
-        await action.click(component.homeScanBtn)
-     })
+   it('[Wording] => Check Upload QR code Btn Text', async () => {
+      // assert
+      await assert.checkText(component.scanUploadText, 'Upload QR code')
+   })
 
-     afterAll(async () => {
-        await action.removeApps(`${process.env.PACKAGE_ID}`)
-     })
+   it('[Wording] => Check Bottom Text', async () => {
+      // assert
+      await assert.checkText(component.scanBottomText, 'Align QR code within frame to scan')
+   })
+
+   it('[Tap] => Check Back Btn Tap', async () => {
+      // action
+      await action.click(component.scanBackBtn)
+
+      // assert
+      await assert.checkElementDisplayed(component.homeScanBtn)
+
+      // after (back to Scan)
+      await action.click(component.homeScanBtn)
+   })
+
+   it('[Tap] => Check Receive Btn Tap', async () => {
+      // action
+      await action.click(component.scanReceiveBtn)
+
+      // assert
+      await assert.checkElementDisplayed(component.selectTokenTitleText)
+
+      // after (back to Scan)
+      await action.click(component.selectTokenBackBtn)
+   })
+
+   it('[Tap] => Check Upload QR code Btn Tap', async () => {
+      // action
+      await action.click(component.scanUploadBtn)
+
+      // action (for handle permission)
+      await action.click(component.photoPermissionAllowBtn1)
+      await action.click(component.photoPermissionAllowBtn2)
+      await action.pause(2000)
+
+      // assert
+      await assert.checkElementDisplayed(component.uploadPopupText)
+
+      // after (back to Scan)
+      await action.click(component.uploadPopupCloseBtn)
+   })
+
+   it('[Function] => Selected upload photo', async () => {
+      // action for add photo
+      await action.click(component.scanReceiveBtn)
+      await action.click(component.selectTokenKUB)
+
+      await action.click(component.receiveDownloadBtn)
+      await action.pause(2000)
+
+      await action.click(component.receiveBackBtn)
+
+      await action.click(component.selectTokenBackBtn)
+
+      // action
+      await action.click(component.scanUploadBtn)
+      await action.pause(2000)
+
+      await action.click(component.uploadPopupPhotoSelected)
+
+      // assert
+      await assert.checkElementDisplayed(component.sendTitleText)
+
+      // after (back to Scan)
+      await action.click(component.sendBackBtn)
+
+      await action.click(component.homeScanBtn)
+   })
+
+   afterAll(async () => {
+      await action.removeApps(`${process.env.PACKAGE_ID}`)
+   })
 })
