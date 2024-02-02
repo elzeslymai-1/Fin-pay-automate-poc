36721285ac1950564a627a3e783577ede9b6f08c
Juggrid <champ_psu@outlook.co.th>
2024-02-02 16:38:41 +0700

    Feature Currency Page iOS ### Add Currency page test case ### Add Currency component ### Edit WDIO iOS config 	- Add currency test case to specs pattern ### Edit Setting test case 	- Add after all flow to test case

---
+++ test/iosTest/component/currencyComponent.ts
@@ -0,0 +1,52 @@
+export class CurrencyComponent {
+    // Menu Bar
+    homeTabHomeBtn = '~Home, tab, 1 of 4'
+    homeTabSettingBtn = '~Setting, tab, 4 of 4'
+    
+
+    // Home Page Component
+    homeBalanceText = '~home-walletsection-balance'
+    homeSendBtn = '~home-actiondock-send'
+    homeReceiveBtn = '~home-actiondock-receive'
+
+    homeTokenCardKubValueText = '~home-walletcardbalancefiat-KUB'
+    homeTokenCardWtkValueText = '~home-walletcardbalancefiat-WTK'
+
+    // Send Page Component
+    sendBackBtn = '~'
+    sendSelectTokenBtn = '-ios class chain:**/XCUIElementTypeOther[`name == "KUB"`]'
+    sendSelectTokenBackBtn = '~'
+
+    sendSelectTokenKUBValueText = '-ios class chain:**/XCUIElementTypeOther[`name == "KUB KUB ¥0  0 KUB"`][2]'
+    sendSelectTokenWTKValueText = '-ios class chain:**/XCUIElementTypeOther[`name == "WTK WToken ¥0  0 WTK"`][2]'
+
+    // Receive Page Component
+    receiveBackBtn = '~'
+
+    receiveSelectTokenKUBValueText = '-ios class chain:**/XCUIElementTypeOther[`name == "KUB KUB ¥0 0 KUB"`][2]'
+    receiveSelectTokenWTKValueText = '-ios class chain:**/XCUIElementTypeOther[`name == "WTK WToken ¥0 0 WTK"`][2]'
+
+    // Setting Page Component
+    settingTitleText = '-ios class chain:**/XCUIElementTypeOther[`name == "Setting"`][5]'
+
+    settingCurrencyUSDBtn = '-ios class chain:**/XCUIElementTypeOther[`name == " Currency USD "`][2]'
+    settingCurrencyJPYBtn = '-ios class chain:**/XCUIElementTypeOther[`name == " Currency JPY "`][2]'
+
+    // Currency Page Component
+    currencyTitleText = '-ios class chain:**/XCUIElementTypeStaticText[`name == "Currency"`]'
+    currencyBackBtn = '~'
+    currencySearchTextField = '~home-walletmodal-search'
+    currencySearchNotfoundText = '-ios class chain:**/XCUIElementTypeStaticText[`name == "Not Found"`]'
+
+    currencyUSDBtn = '-ios class chain:**/XCUIElementTypeOther[`name == "USD - United States Dollar"`][2]'
+    currencyEURBtn = '-ios class chain:**/XCUIElementTypeOther[`name == "EUR - Euro"`][2]'
+    currencyJPYBtn = '-ios class chain:**/XCUIElementTypeOther[`name == "JPY - Japanese Yen"`][2]'
+    currencyCNYBtn = '-ios class chain:**/XCUIElementTypeOther[`name == "CNY - Chinese Yuan"`][2]'
+    currencyTHBBtn = '-ios class chain:**/XCUIElementTypeOther[`name == "THB - Thai Baht"`][2]'
+    currencyGBPBtn = '-ios class chain:**/XCUIElementTypeOther[`name == "GBP - Pound Sterling"`][1]'
+    currencyRUBBtn = '-ios class chain:**/XCUIElementTypeOther[`name == "RUB - Russian Ruble"`][2]'
+    currencyCADBtn = '-ios class chain:**/XCUIElementTypeOther[`name == "CAD - Canadian Dollar"`][2]'
+    currencyTRYBtn = '-ios class chain:**/XCUIElementTypeOther[`name == "TRY - Turkish Lira"`][2]'
+    currencyBRLBtn = '-ios class chain:**/XCUIElementTypeOther[`name == "BRL - Brazilian Real"`][2]'
+    currencyKHRBtn = '-ios class chain:**/XCUIElementTypeOther[`name == "KHR - Riel"`][2]'
+}
